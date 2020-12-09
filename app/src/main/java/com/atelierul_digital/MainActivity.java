20382fa7c364903f0ea5d943353fef29491327f9
alexxmanea <alexmanea2000@gmail.com>
2020-12-09 21:27:52 +0200

    Cursul 2 - tema

--- app/src/main/java/com/atelierul_digital/MainActivity.java
+++ app/src/main/java/com/atelierul_digital/MainActivity.java
@@ -3,6 +3,7 @@ package com.atelierul_digital;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RatingBar ratingBar = findViewById(R.id.ratingbar_1);
+        ratingBar.setRating(4);
     }
 }

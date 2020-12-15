ec4b8118cca865de15ef3d086096ca22ee458993
alexxmanea <alexmanea2000@gmail.com>
2020-12-15 21:20:04 +0200

    Intents - Both way data pass between 2 activities

--- app/src/main/java/com/atelierul_digital/MainActivity.java
+++ app/src/main/java/com/atelierul_digital/MainActivity.java
@@ -1,27 +1,46 @@
 package com.atelierul_digital;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText height_editText = findViewById(R.id.edittext_height);
+
         Button navigate = findViewById(R.id.navigate);
         navigate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, NewActivity.class);
-                intent.putExtra("SomeInfo", "logInfo");
-                startActivity(intent);
+                intent.putExtra("height", height_editText.getText().toString());
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            String result = data.getStringExtra("height_result");
+            Toast.makeText(this, "Was my height enough? - " + result, Toast.LENGTH_SHORT).show();
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(this, "Invalid height", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/atelierul_digital/NewActivity.java
+++ app/src/main/java/com/atelierul_digital/NewActivity.java
@@ -1,24 +1,58 @@
 package com.atelierul_digital;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewActivity extends AppCompatActivity {
 
+    private String height;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_activity);
 
-        String logInfo = null;
         if (getIntent().getExtras() != null) {
-                logInfo = getIntent().getStringExtra("SomeInfo");
+            height = getIntent().getStringExtra("height");
         }
 
-        Log.i("Lifecycle", "onCreate -> " + logInfo);
+
+        Button analyze_button = findViewById(R.id.analyze_button);
+        TextView analyze_height_textview = findViewById(R.id.analyze_height_textview);
+
+        analyze_height_textview.setText(height);
+
+        analyze_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int heightInCm = Integer.valueOf(height);
+
+                    String isEnough = (heightInCm > 185) ? "YES" : "NO";
+
+
+                    Log.i("Lifecycle", "se apeleaza" + isEnough);
+
+                    Intent intent = new Intent();
+                    intent.putExtra("height_result", isEnough);
+                    setResult(Activity.RESULT_OK, intent);
+
+                    finish();
+                } catch (NumberFormatException e) {
+                    setResult(Activity.RESULT_CANCELED);
+                    finish();
+                }
+            }
+        });
+        Log.i("Lifecycle", "onCreate -> " + height);
     }
 
     @Override

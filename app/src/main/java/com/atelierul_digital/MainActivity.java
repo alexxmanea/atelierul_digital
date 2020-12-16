1863dbcecfba4853329ba1828012c58d94214685
alexxmanea <alexmanea2000@gmail.com>
2020-12-16 04:54:54 +0200

    Challenge 3 - completed
    ConstraintLayout

--- app/src/main/java/com/atelierul_digital/MainActivity.java
+++ app/src/main/java/com/atelierul_digital/MainActivity.java
@@ -1,8 +1,8 @@
 package com.atelierul_digital;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 

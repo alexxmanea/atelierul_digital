143b92cd871a7b876bdb4d05f18621a1b3af3278
alexxmanea <alexmanea2000@gmail.com>
2021-01-19 20:48:26 +0200

    Cursul 7 - Material design

--- app/src/main/java/com/atelierul_digital/MainActivity.java
+++ app/src/main/java/com/atelierul_digital/MainActivity.java
@@ -1,8 +1,15 @@
 package com.atelierul_digital;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,6 +17,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements PersonListener {
     private RecyclerViewAdapter recyclerViewAdapter;
+    private static final int MY_CAMERA_PERMISSION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,33 @@ public class MainActivity extends AppCompatActivity implements PersonListener {
 
         DataSource dataSource = new DataSource(this);
         dataSource.getPersons(this);
+
+        findViewById(R.id.floating_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION);
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_LONG).show();
+                    // Do something with camera
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1000) {
+            switch (permissions[0]) {
+                case Manifest.permission.CAMERA:
+                    if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        // Do something with camera
+                    }
+                    break;
+            }
+        }
     }
 
     @Override
